5f57a77d15abd739e2cf22f873f2856ffc3cdf58
Pascal <raosp@tcd.ie>
2022-03-18 12:40:11 +0000

    test

---
+++ dataManipulation.py
@@ -0,0 +1,46 @@
+#Stage 1, preparing the data
+import numpy as np 
+import pandas as pd 
+from sklearn.utils import resample
+import os
+
+
+#
+#Function that loads in training and testing data
+#
+def readECGData ():
+    
+    current_directory = os.getcwd()
+    print(current_directory)    
+
+
+    train = pd.read_csv('C:/Users/Pascal Raos/Desktop/ECG Project/data/mitbih_train.csv', header=None)
+    test = pd.read_csv('C:/Users/Pascal Raos/Desktop/ECG Project/data/mitbih_test.csv', header=None)
+
+    return train, test
+
+
+def resampleData (data):
+    
+    data0 = (data[data[187]==0]).sample(n=20000,random_state=420)
+    data1 = data[data[187]==1]
+    data2 = data[data[187]==2]
+    data3 = data[data[187]==3]
+    data4 = data[data[187]==4]
+
+    data1up = resample(data1,replace=True,n_samples=20000,random_state=10)
+    data2up = resample(data2,replace=True,n_samples=20000,random_state=11)
+    data3up = resample(data3,replace=True,n_samples=20000,random_state=12)
+    data4up = resample(data4,replace=True,n_samples=20000,random_state=13)
+
+    result = pd.concat([data1up,data2up,data3up,data4up])
+
+    return result
+
+
+#function that returns frequencies of all classes from a dataset
+def frequencyClasses (data):
+    data[187]=data[187].astype(int)
+    equilibre=data[187].value_counts()
+    print(equilibre)
+
---
+++ main.py
@@ -0,0 +1,34 @@
+
+
+#importing a function from the other file
+from keras.utils.np_utils import to_categorical
+
+from dataManipulation import readECGData 
+from dataManipulation import frequencyClasses as getFrequency
+from dataManipulation import resampleData
+
+train, test = readECGData()
+
+#format for accessing pandas dataframes
+#dataframes = var[column][row]
+    # for column in train:
+    #      print(train[column][86789]) # prints all values in row 1pyth
+
+#model([train_inputs, train_outputs],[test_inputs, test_outputs])
+#separate training input and output for model
+    # train_outputs = to_categorical(train[187])
+    # train_inputs = train.iloc[:,:186].values
+
+    # print(train_outputs)
+    # print(train_inputs[85427])
+
+
+
+#below does the resampling for imbalanced data and prints frequencies
+    #resampleData(train)
+
+    #getFrequency(train)
+
+
+
+        
