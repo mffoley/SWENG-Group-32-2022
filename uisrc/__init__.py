6aadfb4c9b32e0406869a6c987ad3835668af48f
Maryann Foley <maryann.v.foley@gmail.com>
2022-03-19 17:31:52 +0000

    add flask sample

---
+++ uisrc/__init__.py
@@ -0,0 +1,19 @@
+
+
+from flask import Flask, redirect, url_for, render_template, session, request, flash, get_flashed_messages
+
+
+app = Flask(__name__)
+@app.route("/")
+def home():
+    return render_template("welcome.html", name="name")
+
+@app.route("/conic")
+def conic():
+    print("data")
+    return redirect(url_for("home"))
+
+if __name__ == "__main__":
+    app.debug = True
+
+app.run()
