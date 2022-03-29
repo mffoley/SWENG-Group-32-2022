715e7b080673ec44c9e5e62b7f3f48c0f5646d43
Maryann Foley <maryann.v.foley@gmail.com>
2022-03-29 17:19:14 +0100

    reformatting data

--- uisrc/__init__.py
+++ uisrc/__init__.py
@@ -9,13 +9,16 @@ ALLOWED_EXTENSIONS = {'csv'}
 app = Flask(__name__)
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
+models = [{"name":"Simple Model", "acc":99.5}]
+fields = ["normal", "var2", "var3", "var4","var5"]
+
 def allowed_file(filename):
     return '.' in filename and \
            filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
 def use_model(input, modelnum):
     print(input)
-    return {"normal":True,'model_name':"Sample Model",'model_acc':99.5}
+    return [{"normal":True,"var2":False,"var3":True,"var4":False,"var5":False},{"normal":True,"var2":False,"var3":True,"var4":False,"var5":False}]
 
 
 @app.route("/")
@@ -34,12 +37,12 @@ def check():
     print(list(request.form.keys()))
     if "file" in request.files.keys() and allowed_file(request.files['file'].filename):
         f = request.files['file']
-
+        model_info = models[0]
         # vvvvvvv do something with the file data vvvvvvv
         data = use_model(f.read(),1)
         # ^^^^^^^ do something with the file data ^^^^^^^
 
-        return render_template("results.html", model_name=data["model_name"] , model_accuracy=data["model_acc"], normal = data["normal"] )
+        return render_template("results.html", model_name=model_info["name"] , model_accuracy=model_info["acc"], fields=fields, results = data )
 
     return redirect(url_for('home'))
 
